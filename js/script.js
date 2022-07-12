129d65853f8baa68f042a8cbfeed29e2541174ff
code8d <code8d@gmail.com>
2022-07-12 17:06:54 +0500

    Small bug fixed

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const result = document.querySelector('input');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+        if (btn.classList.contains('btn_result')) {
+            if (result.value.includes('document')) {
+                result.value = 0;
+            } else result.value = eval(result.value);
+        }   else result.value += btn.innerHTML;
+    })
+});
